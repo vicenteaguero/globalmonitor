a80cee1bf855181b2929738715f371bd8bcf8c56
Vicente Agüero <vicenteaguero@uc.cl>
2025-01-08 00:08:41 -0300

    feat(streamlit): :sparkles: Added Custom Layouts and Nav for Streamlit

---
+++ streamlit/src/layout.py
@@ -0,0 +1,39 @@
+# streamlit/src/layout.py
+
+import streamlit as st
+
+
+from src.params import PATHS, GITHUB_URL, ABOUT_URL
+
+def setup_layout(page_title: str, page_icon: str):
+    st.set_page_config(
+        layout='wide',
+        page_title=page_title,
+        initial_sidebar_state='expanded',
+        menu_items={
+            'Get help': GITHUB_URL,
+            'Report a bug': GITHUB_URL+'/issues',
+            'About': ABOUT_URL
+        }
+    )
+    st.logo(image=PATHS['logo-dark'], link=GITHUB_URL, size='large')
+
+def setup_pages():
+    pages = {
+        'Home': [
+            st.Page(page=PATHS['pages']['home'], title='Home', icon='🏠'),
+            st.Page(page=PATHS['pages']['docs'], title='Docs', icon='📚'),
+        ],
+        'Global Monitor': [
+            st.Page(page=PATHS['pages']['paginator'], title='Paginator', icon='📰'),
+            st.Page(page=PATHS['pages']['formatter'], title='Format Pages', icon='🗞️'),
+        ],
+    }
+    nav = st.navigation(pages)
+    nav.run()
+
+def setup_custom_css():
+    styles = ['base']
+    for style in styles:
+        with open(PATHS[f'{style}_css']) as f:
+            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
