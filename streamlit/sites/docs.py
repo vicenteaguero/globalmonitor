706b17159a1de0372a3d5962f68a47a677a6aff9
Vicente Agüero <vicenteaguero@uc.cl>
2025-01-08 00:09:17 -0300

    feat(streamlit): :sparkles: Added placeholder Streamlit sites

---
+++ streamlit/sites/docs.py
@@ -0,0 +1,6 @@
+# streamlit/sites/docs.py
+
+from src.layout import setup_layout, setup_custom_css
+
+setup_layout(page_title='Docs', page_icon='📚')
+setup_custom_css()
---
+++ streamlit/sites/home.py
@@ -0,0 +1,6 @@
+# streamlit/sites/home.py
+
+from src.layout import setup_layout, setup_custom_css
+
+setup_layout(page_title='Home', page_icon='🏠')
+setup_custom_css()
